daa57a8b578b644bdd4b1cb66ec6b91253deef48
Lewis López <lopezsoft.com@gmail.com>
2020-08-27 13:14:06 -0500

    Variable envoiroment

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  APIURL    : 'https://y-code/api/v1',
+  APPURL    : 'https://y-code',
+  APIJWT    : 'y-code-jwt'
 };
 
 /*
